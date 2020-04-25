875bc7635ca0c52442d3c1d2541b8b3413007f00
kgho <kuanggh@outlook.com>
2020-04-25 15:12:41 +0800

    计分板UI素材更新，对战界面UI

---
+++ Source/RainbowSeven_C/UI/CombatWidget.h
@@ -0,0 +1,17 @@
+// Author : Kgho	Github : https://github.com/kgho
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAINBOWSEVEN_C_API UCombatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
